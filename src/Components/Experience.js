fcbbfdd7f02620ca24e6bcf340e7faf0c846ff6f
Aniket Shahi <109075043+aniket0608@users.noreply.github.com>
2024-08-11 17:36:59 +0530

    Experience Update

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -50,17 +50,35 @@ const Experience = () => {
         />
 
         <ul className="w-full flex flex-col items-start justify-between xs:ml-2">
-         <Details
+          <Details
             type="Alpha MLSA "
             time="Oct 2023 - Present"
             place="Micosoft"
             info="In my journey with MLSA (Microsoft Learn Student Ambassadors), achieving the ALPHA level is a major accomplishment. It means I've made significant progress, gained expertise, and actively contributed to the MLSA community. This milestone reflects my commitment and growth within the program. "
           />
-         <Details
+          <Details
             type="Co Lead "
             time="Aug 2023 - Present "
             place="GDSC - National Forensic Sciences University Delhi"
             info="I am excited to be the Google Developer Student Clubs Lead at National Forensic Sciences University - Delhi. In this role, I will be responsible for starting and growing a community of students interested in Google developer technologies. I will also be organizing workshops and events to help students learn about these technologies and build their skills."
+          />
+          <Details
+            type="Cybersecurity Intern (GPCSSI24)"
+            time="Jun 2024 - Jul 2024"
+            place="Haryana Police"
+            info="- Participated in real-world cybercrime investigations, learning the latest methods and techniques.Gained expertise in Dark Web OSINT, digital forensics, and the modes of operation for various cybercrimes.Collaborated with central government agencies on cybersecurity initiatives.Developed and implemented a CDR Analysis System project to assist in criminal investigations.Explored cutting-edge technologies such as blockchain and quantum computing for cybersecurity applications.Acquired practical knowledge in bug hunting and cyber law enforcement"
+          />
+          <Details
+            type="AI and Cloud Intern"
+            time="Jun 2024 - Jul 2024 "
+            place="Edunet Foundation"
+            info="- IBM SkillsBuild & Cloud Platform: Leveraged the IBM SkillsBuild e-Learning Platform for industry-relevant courses in AI and Cloud technologies.Collaborative Project: Developed a predictive analysis project on TED Talk views using Python, IBM Watson, and IBM Cloud, working in a team with mentor guidance.Technical Skills Development: Utilized data analysis and machine learning techniques, cloud computing, and API integration to solve real-world challenges.Industry Tools: Gained proficiency in tools such as Jupyter Notebooks, IBM Watson Studio, and IBM Cloud services."
+          />
+          <Details
+            type="Webacy Web3 Security Data Analytics Externship"
+            time="Jun 2024 - Jul 2024 "
+            place="Extern"
+            info="Conducted detailed analysis and labeling of smart contract vulnerabilities, enhancing skills in data categorization and risk assessment.Validated peers' data labels to ensure accuracy and reliability of data sets.Performed frequency and correlation analysis of risk tags to identify the most prevalent vulnerabilities and their associations, gaining proficiency in data manipulation and statistical tools.Utilized unsupervised machine learning techniques for cluster analysis, discovering patterns and commonalities among smart contracts to profile risks effectively."
           />
           <Details
             type="InSIG'23 Fellow "
